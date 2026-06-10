40ce6b6fc2c3fb426331b0c180e056bfbfa97713
zn1ckl3ss <mr.n1ckl3ss@gmail.com>
2026-06-10 23:30:19 +0300

    feat: add dockerfile, docker-compose and monitoring

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Safe IS Project API")
 
+# Инициализируем сборщик метрик при старте приложения
+@app.on_event("startup")
+async def startup():
+    Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def read_root():
