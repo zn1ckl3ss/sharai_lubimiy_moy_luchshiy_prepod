b45a1ebbcf3fe2de134938f4d556c4c676002630
zn1ckl3ss <mr.n1ckl3ss@gmail.com>
2026-06-11 01:32:57 +0300

    megatest pj zapustis 3

--- app/main.py
+++ app/main.py
@@ -3,10 +3,11 @@ from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Safe IS Project API")
 
-# Инициализируем сборщик метрик при старте приложения
+
 @app.on_event("startup")
 async def startup():
     Instrumentator().instrument(app).expose(app)
+
 
 @app.get("/")
 def read_root():
